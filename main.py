b19773937e4c6c4061affa17f663249c9c68d3ce
vishwait <vishwajeet.mall@bee-logical.com>
2025-07-17 11:56:37 +0530

    Fix cors issue - by adding origin's in main

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.zoho import auth, folders
 
 app = FastAPI()
+
+# Add this block to enable CORS
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register routers
 app.include_router(auth.router)
@@ -9,4 +24,4 @@ app.include_router(folders.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True) #local
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
